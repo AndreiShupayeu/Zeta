f8b4c5a6c03587097b2925b73cb978de98b033fe
AndreiShupayeu <shupaevandrei@tut.by>
2021-11-21 18:09:19 +0300

    Add testVacancies

--- src/test/java/Zetreex.java
+++ src/test/java/Zetreex.java
@@ -40,5 +40,25 @@ public class Zetreex {
         System.out.println(wordInRussian.getText());
         Assert.assertEquals(wordInRussian.getText(), "О нас");
     }
+    @Test
+    public void testVacancies(){
+        WebElement vacancies=driver.findElement(By.xpath("//div[@id=\"app\"]/header/div/div/nav/ul/li[2]/a/span"));
+        vacancies.click();
+        WebElement name= driver.findElement(By.xpath("//input[@id=\"vacancies__name1\"]"));
+        name.sendKeys("Name");
+        WebElement position = driver.findElement(By.xpath("//div[@id=\"app\"]/div[2]/div/div[2]/form/div[2]"));
+        position.click();
+        WebElement oneOfPosition = driver.findElement(By.xpath("//div[@id=\"app\"]/div[2]/div/div[2]/form/div[2]/div[3]/p[1]"));
+        oneOfPosition.click();
+        WebElement fonOrLinkedin=driver.findElement(By.xpath("//input[@id=\"vacancies__linked1\"]"));
+        fonOrLinkedin.sendKeys("12345");
+        WebElement approval = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div[2]/form/div[4]/label[1]"));
+        approval.click();
+        WebElement toSend=driver.findElement(By.xpath("//div[@id=\"app\"]/div[2]/div/div[2]/form/button"));
+        toSend.click();
+        WebElement popap=driver.findElement(By.xpath("//div[@class='modal']//button"));
+        Assert.assertEquals(popap.getText(),"Хорошо");
+
+    }
 }
 
